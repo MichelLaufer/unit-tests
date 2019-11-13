1b19824415c6ff23b4a8b697d6b9b21d0e1ac6cc
Michel Elmoznino Laufer <mitchel_85@hotmail.com>
2019-11-13 18:20:40 +0100

    Unit tests number 1 to 7

--- src/5-month-name.js
+++ src/5-month-name.js
@@ -4,5 +4,41 @@ const months = [
 ]
 
 export const monthName = (monthNumber) => {
-  return months[monthNumber]
+  if(monthNumber < 1 || monthNumber > 12) {
+    return null
+  }
+  return months[monthNumber - 1] 
+
+  // if (monthNumber[0]) {
+  //   return months[monthNumber]
+  // } else if (monthNumber[1]) {
+  //   return months[monthNumber] 
+  // } else if (monthNumber[2]) {
+  //   return months[monthNumber] 
+  // } else if (monthNumber[3]) {
+  //   return months[monthNumber] 
+  // } else if (monthNumber[4]) {
+  //   return months[monthNumber] 
+  // } else if (monthNumber[5]) {
+  //   return months[monthNumber] 
+  // } else if (monthNumber[6]) {
+  //   return months[monthNumber] 
+  // } else if (monthNumber[7]) {
+  //   return months[monthNumber] 
+  // } else if (monthNumber[8]) {
+  //   return months[monthNumber] 
+  // } else if (monthNumber[9]) {
+  //   return months[monthNumber] 
+  // } else if (monthNumber[10]) {
+  //   return months[monthNumber] 
+  // } else if (monthNumber[11]) {
+  //   return months[monthNumber] 
+  // } else if (monthNumber[12]) {
+    
+
+  // months.forEach 
+  // return months[monthNumber]
+  // if (monthNumber[0]) {}
+  // return months[monthNumber]
 }
+
